4bd69d4acf703861b208a7cc1201f1a4a54f6689
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-14 11:54:53 +0100

    Solved a problem with SingleProduct component and giving functionality to Mas Informacion button in Producto component

--- src/Componentes/SingleProducto.js
+++ src/Componentes/SingleProducto.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import '../css/SingleProducto.css';
+import Error from "./Error";
 
 const SingleProducto = (props) => {
-    if (props) return null;
-    const {imagen, nombre, precio, descripcion} = props.producto;
-    return (
-        <div className="info-producto">
-            <div className="imagen">
-                <img src={`/img/${imagen}.png`} alt={nombre}/>
+
+    if (!props || props.producto === undefined) {
+        return (
+            <Error/>
+            /* aca aprovecho el componente Error */
+        )
+    } else if (props && props.producto !== undefined) {
+
+
+        const {imagen, nombre, precio, descripcion} = props.producto;
+
+        return (
+            <div className="info-producto">
+                <div className="imagen">
+                    <img src={`/img/${imagen}.png`} alt={nombre}/>
+                </div>
+                <div className="info">
+                    <h2>{nombre}</h2>
+                    <p className="precio">${precio}</p>
+                    <p>{descripcion}</p>
+                </div>
             </div>
-            <div className="info">
-                <h2>{nombre}</h2>
-                <p className="precio">${precio}</p>
-                <p>{descripcion}</p>
-            </div>
-        </div>
-    )
+        )
+    }
 };
 
 export default SingleProducto;
