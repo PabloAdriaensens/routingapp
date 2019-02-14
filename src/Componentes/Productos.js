3a2a77674823f3d3236d631844d689229031d670
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-14 14:25:08 +0100

    Added a product search in Buscador component

--- src/Componentes/Productos.js
+++ src/Componentes/Productos.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import Producto from "./Producto";
 import '../css/Productos.css';
+import Buscador from "./Buscador";
 
 class Productos extends Component {
     render() {
         return (
             <div className="productos">
                 <h2>Nuestros productos</h2>
+                <Buscador
+                    busqueda={this.props.busquedaProducto}
+                />
                 <ul className="lista-productos">
                     {Object.keys(this.props.productos).map
                     (producto => (
